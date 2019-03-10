3028bd260ca00d7d381f7ba1afe1cb62adf3afbc
Jonny McFadden <jmcfadden1528@gmail.com>
2019-03-10 13:33:06 -0500

    single line array in progress

--- SingleLineArray/Program.cs
+++ SingleLineArray/Program.cs
@@ -8,6 +8,33 @@ namespace SingleLineArray
         {
             // TODO: Initialize a single line an array with the following values: 1, 1, 2, 3, 5, 8. 
 
+
+            int[] numbers = new int[6];
+
+            //numbers[0] = 1;
+            //numbers[1] = 1;
+            //numbers[2] = 2;
+            //numbers[3] = 3;
+            //numbers[4] = 5;
+            //numbers[5] = 8;
+
+            foreach (int value in numbers)
+            {
+                //int[] numbers = new int[6];
+
+                numbers[0] = 1;
+                numbers[1] = 1;
+                numbers[2] = 2;
+                numbers[3] = 3;
+                numbers[4] = 5;
+                numbers[5] = 8;
+
+
+                Console.WriteLine();
+                //Console.ReadLine();
+            }
+
+
             // TODO: Loop through the array using foreach and print out each value on its own line.
 
             Console.ReadLine();
