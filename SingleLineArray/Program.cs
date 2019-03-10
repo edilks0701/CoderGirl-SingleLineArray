9751cf04d358218e39e3cca425babbb770c8d108
Jonny McFadden <jmcfadden1528@gmail.com>
2019-03-10 18:42:21 -0500

    completed single line array

--- SingleLineArray/Program.cs
+++ SingleLineArray/Program.cs
@@ -8,30 +8,19 @@ namespace SingleLineArray
         {
             // TODO: Initialize a single line an array with the following values: 1, 1, 2, 3, 5, 8. 
 
-
-            int[] numbers = new int[6];
-
-            //numbers[0] = 1;
-            //numbers[1] = 1;
-            //numbers[2] = 2;
-            //numbers[3] = 3;
-            //numbers[4] = 5;
-            //numbers[5] = 8;
+            int[] numbers = {1,1,2,3,5,8};
+            
+            numbers[0] = 1;
+            numbers[1] = 1;
+            numbers[2] = 2;
+            numbers[3] = 3;
+            numbers[4] = 5;
+            numbers[5] = 8;
 
             foreach (int value in numbers)
             {
-                //int[] numbers = new int[6];
-
-                numbers[0] = 1;
-                numbers[1] = 1;
-                numbers[2] = 2;
-                numbers[3] = 3;
-                numbers[4] = 5;
-                numbers[5] = 8;
-
-
-                Console.WriteLine();
-                //Console.ReadLine();
+                   
+                 Console.WriteLine(value);
             }
 
 
